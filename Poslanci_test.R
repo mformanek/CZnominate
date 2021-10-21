3e2cc865b273d56452429122d3a528f257bb9a0f
mformanek <mourrison@live.com>
2021-10-20 23:03:43 -0700

    more testing

--- Poslanci_test.R
+++ Poslanci_test.R
@@ -31,16 +31,10 @@ download_poslanci()
 
 poslanci_test <- merge(osoby,osoba_extra, by="id_osoba") #merge osoby table with osoba_extra to get party info
 poslanci_test <- merge(poslanci_test,poslanec,by="id_osoba")
-poslanci_test<-poslanci_test[c("id_osoba","jmeno","prijmeni","strana")] #delete unused info (columns)
+poslanci_test<-poslanci_test[c("id_poslanec","jmeno","prijmeni","strana")] #delete unused info (columns)
 poslanci_test$jmeno<-paste(poslanci_test$jmeno, poslanci_test$prijmeni) # concat jmeno and prijimeni columns
-poslanci_test<-poslanci_test[!duplicated(poslanci_test),] #de-duplicate table
-rownames(poslanci_test) <- NULL #renumber rows
-poslanci_test <- merge(poslanci_test,poslanec,by="id_osoba") #merge osoby table with osoba_extra to get party info
+poslanci_test<-poslanci_test[c("id_poslanec","jmeno","strana")] #delete last name (column)
+#poslanci_test<-poslanci_test[!duplicated(poslanci_test),] #de-duplicate table
+#rownames(poslanci_test) <- NULL #renumber rows
 
-poslanci_test <- merge(osoby,poslanec,by="id_osoba")
-#organy_test<-organy[organy$id_typ_organu == "6" | organy$id_typ_organu == "1",]
-
-zarazeni_test<-zarazeni[zarazeni$cl_funkce == "0",] #vybrat jenom clenstvi
-merging_test<-merge(zarazeni_test, organy, by.x=c("id_of"),by.y=c("id_organ"))
-merging_test2<-merge(poslanci_test, merging_test, by="id_osoba")
 ##end test code##
